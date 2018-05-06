c274cdcf5ba6bc4f79f8abb9a2bf2b7f532114d5
zhenkai.zhang <zhenkai.zhangSH@downtown8.com>
2018-05-06 19:11:00 +0800

    fix “

--- src/views/com/SideBar.vue
+++ src/views/com/SideBar.vue
@@ -27,10 +27,10 @@
           key = "model.id"
         />
         <li><a href="https://adminlte.io/themes/AdminLTE/documentation/index.html"><i class="fa fa-book"></i> <span>Documentation</span></a></li>
-        <!-- <li class="header">LABELS</li>
+        <li class="header">LABELS</li>
         <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Important</span></a></li>
         <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> <span>Warning</span></a></li>
-        <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>Information</span></a></li> -->
+        <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>Information</span></a></li>
       </ul>
     </section>
   </aside>
